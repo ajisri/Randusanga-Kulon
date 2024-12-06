0b9c6597dfef8fee1e6b2bd9349ab186a20e71e6
Ajis <ajisriyananta@gmail.com>
2024-12-07 01:41:26 +0700

    cuk3

--- frontend/src/components/Administator/Transparansi/Subkategoriankor.jsx
+++ frontend/src/components/Administator/Transparansi/Subkategoriankor.jsx
@@ -218,7 +218,7 @@ const SubkategoriAnkor = () => {
         );
         console.log(
           "🚀 ~ subkategoriResponse.data.uuid boy 3:",
-          subkategoriResponse.data?.uuid
+          subkategoriResponse.data.data.uuid
         );
 
         // Menyiapkan payload untuk update atau tambah poin
