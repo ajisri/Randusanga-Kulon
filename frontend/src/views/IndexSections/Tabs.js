f24ad202a9b4cbe1235fdd72627c1371e0a15976
Ajis <ajisriyananta@gmail.com>
2025-01-11 11:12:13 +0700

    updatelokal

--- frontend/src/views/IndexSections/Tabs.js
+++ frontend/src/views/IndexSections/Tabs.js
@@ -15,164 +15,122 @@ import Modalt from "./Modalt";
 
 class TabsSection extends React.Component {
   state = {
-    iconTabs: 1,
     plainTabs: 1,
+    pieces: {
+      1: [],
+      2: [],
+      3: [],
+    },
+    numPieces: 150,
+    animationKeys: {
+      1: Date.now(),
+      2: Date.now(),
+      3: Date.now(),
+    },
   };
 
   toggleNavs = (e, state, index) => {
     e.preventDefault();
+
     this.setState({
       [state]: index,
+      animationKeys: {
+        ...this.state.animationKeys,
+        [index]: Date.now(),
+      },
+      pieces: {
+        ...this.state.pieces,
+        [index]: this.generatePieces(index),
+      },
     });
   };
 
-  render() {
-    const isMobile = window.innerWidth <= 768;
+  generatePieces = (index) => {
+    const pieces = [];
+    const centerX = 50;
+    const centerY = 50;
 
-    const tabStyle = {
-      color: "white",
-      marginLeft: isMobile ? "35px" : "35px",
+    for (let i = 0; i < this.state.numPieces; i++) {
+      const distanceFromCenter = Math.random();
+      const angle = Math.random() * 2 * Math.PI;
+
+      const offsetX = centerX + Math.cos(angle) * distanceFromCenter * 50;
+      const offsetY = centerY + Math.sin(angle) * distanceFromCenter * 50;
+
+      pieces.push({
+        id: i,
+        style: this.structuredPieceStyle(offsetX, offsetY, distanceFromCenter),
+      });
+    }
+    return pieces.sort((a, b) =>
+      a.style.animationDelay.localeCompare(b.style.animationDelay)
+    );
+  };
+
+  structuredPieceStyle = (x, y, distanceFactor) => {
+    return {
+      position: "absolute",
+      width: `${Math.random() * 10 + 5}px`,
+      height: `${Math.random() * 10 + 5}px`,
+      background: `rgba(255, 105, 180, ${Math.random() * 0.5 + 0.5})`,
+      top: `${y}%`,
+      left: `${x}%`,
+      transform: `translate(-50%, -50%)`,
+      opacity: 1,
+      animation: `fly-out 1s forwards`,
+      animationDelay: `${distanceFactor}s`,
     };
+  };
 
+  render() {
     return (
       <>
         <style>
           {`
-            .nav-link {
+            .futuristik-nav-link {
               position: relative;
-              display: inline-block;
-              font-family: 'Orbitron', sans-serif;
-              font-size: 1.1rem;
-              text-transform: uppercase;
-              text-decoration: none;
-              color: #fffa65;
-              border: 2px solid transparent;
-              padding: 10px 20px;
-              border-radius: 10px;
-              background: transparent;
-              background-color: transparent;
               overflow: hidden;
-              z-index: 1;
-              transition: background 0.3s ease, color 0.3s ease;
+              display: inline-block;
+              width: 100%;
+              padding: 15px 25px;
+              font-size: 1.2rem;
+              text-align: center;
+              border-radius: 12px;
+              color: #808080 !important;
+              background: linear-gradient(135deg, #2c2c54, #40407a);
+              border: 1px solid #f5f5f5;
+              transition: all 0.4s ease-in-out;
             }
 
-            @keyframes splitEffect {
+            .futuristik-nav-link.active {
+              color: #000000 !important;
+              background: #ffffff !important;
+              border-color: transparent !important;
+              box-shadow: 0 0 10px rgba(255, 255, 255, 0.6),
+                          0 0 20px rgba(255, 255, 255, 0.5) !important;
+            }
+
+            .futuristik-nav-link:hover {
+              background: rgba(255, 255, 255, 0.1);
+              color: rgb(58, 57, 57) !important;
+              border: 2px solid #fffa65;
+              box-shadow: 0 0 15px rgba(255, 255, 255, 0.8),
+                          0 0 25px rgba(238, 130, 238, 0.7),
+                          0 0 35px rgba(255, 215, 0, 0.6),
+                          0 0 45px rgba(0, 255, 255, 0.7);
+              transition: all 0.5s ease-in-out;
+            }
+
+            @keyframes fly-out {
               0% {
-                transform: scale(1);
-                clip-path: inset(0 0 0 0); /* Utuh, tidak terpecah */
                 opacity: 1;
-                background-color: transparent;
-              }
-              25% {
-                transform: scale(1);
-                clip-path: inset(0 25% 0 25%); /* Mulai pecah dari tengah */
-                opacity: 0.9;
-                background-color: transparent;
-              }
-              50% {
-                transform: scale(1);
-                clip-path: inset(0 50% 0 50%); /* Pecah menjadi dua bagian dari tengah */
-                opacity: 0.8;
-                background-color: transparent;
-              }
-              75% {
-                transform: scale(1);
-                clip-path: inset(0 75% 0 75%); /* Terbagi lebih jauh */
-                opacity: 0.6;
-                background-color: transparent;
+                transform: translate(0, 0) scale(1);
               }
               100% {
-                transform: scale(1);
-                clip-path: inset(0 0 0 0); /* Kembali ke bentuk pecah penuh */
-                opacity: 1; /* Hilang sepenuhnya pada akhir animasi */
-                background-color: transparent;
+                opacity: 0;
+                transform: translate(calc(100px * (rand(-1, 1))), calc(100px * (rand(-1, 1)))) scale(0.5);
               }
             }
-
-             @keyframes neonBorderEffect {
-      0% {
-        border-top-color: rgba(255, 255, 255, 0.8); /* Putih lembut */
-        border-right-color: rgba(144, 238, 144, 0.8); /* Hijau pastel */
-        border-bottom-color: rgba(255, 215, 0, 0.8); /* Emas lembut */
-        border-left-color: rgba(255, 140, 0, 0.8); /* Oranye terang */
-        box-shadow: 0 0 20px rgba(255, 255, 255, 0.8), 0 0 30px rgba(144, 238, 144, 0.8), 0 0 40px rgba(255, 215, 0, 0.8), 0 0 50px rgba(255, 140, 0, 0.8), 0 0 60px rgba(238, 130, 238, 0.8);
-      }
-      25% {
-        border-top-color: rgba(144, 238, 144, 0.8); /* Hijau pastel */
-        border-right-color: rgba(255, 215, 0, 0.8); /* Emas lembut */
-        border-bottom-color: rgba(255, 140, 0, 0.8); /* Oranye terang */
-        border-left-color: rgba(238, 130, 238, 0.8); /* Ungu pastel */
-        box-shadow: 0 0 20px rgba(144, 238, 144, 0.8), 0 0 30px rgba(255, 215, 0, 0.8), 0 0 40px rgba(255, 140, 0, 0.8), 0 0 50px rgba(238, 130, 238, 0.8), 0 0 60px rgba(255, 255, 255, 0.8);
-      }
-      50% {
-        border-top-color: rgba(255, 215, 0, 0.8); /* Emas lembut */
-        border-right-color: rgba(255, 140, 0, 0.8); /* Oranye terang */
-        border-bottom-color: rgba(238, 130, 238, 0.8); /* Ungu pastel */
-        border-left-color: rgba(255, 255, 255, 0.8); /* Putih lembut */
-        box-shadow: 0 0 20px rgba(255, 215, 0, 0.8), 0 0 30px rgba(255, 140, 0, 0.8), 0 0 40px rgba(238, 130, 238, 0.8), 0 0 50px rgba(255, 255, 255, 0.8), 0 0 60px rgba(144, 238, 144, 0.8);
-      }
-      75% {
-        border-top-color: rgba(255, 140, 0, 0.8); /* Oranye terang */
-        border-right-color: rgba(238, 130, 238, 0.8); /* Ungu pastel */
-        border-bottom-color: rgba(255, 255, 255, 0.8); /* Putih lembut */
-        border-left-color: rgba(144, 238, 144, 0.8); /* Hijau pastel */
-        box-shadow: 0 0 20px rgba(255, 140, 0, 0.8), 0 0 30px rgba(238, 130, 238, 0.8), 0 0 40px rgba(255, 255, 255, 0.8), 0 0 50px rgba(144, 238, 144, 0.8), 0 0 60px rgba(255, 215, 0, 0.8);
-      }
-      100% {
-        border-top-color: rgba(238, 130, 238, 0.8); /* Ungu pastel */
-        border-right-color: rgba(255, 255, 255, 0.8); /* Putih lembut */
-        border-bottom-color: rgba(144, 238, 144, 0.8); /* Hijau pastel */
-        border-left-color: rgba(255, 215, 0, 0.8); /* Emas lembut */
-        box-shadow: 0 0 20px rgba(238, 130, 238, 0.8), 0 0 30px rgba(255, 255, 255, 0.8), 0 0 40px rgba(144, 238, 144, 0.8), 0 0 50px rgba(255, 215, 0, 0.8), 0 0 60px rgba(255, 140, 0, 0.8);
-      }
-    }
-
-     /* Menghindari warna latar belakang biru pada saat fokus */
-    .nav-link:focus {
-      outline: none; /* Menonaktifkan outline biru default pada fokus */
-      background-color: transparent; /* Menjaga transparansi */
-    }
-
-    .nav-link.active {
-      background-color: transparent; /* Pastikan tetap transparan saat aktif */
-      content: '';
-      position: absolute;
-      top: -5px;
-      left: -5px;
-      right: -5px;
-      bottom: -5px;
-      border-radius: 12px; /* Sama dengan tombol */
-      border: 2px solid transparent; /* Border tetap transparan */
-      animation: neonBorderEffect 3s linear infinite; /* Animasi border */
-      pointer-events: none; /* Mencegah interaksi dengan pseudo-element */
-      box-shadow: 
-        0 0 20px rgba(255, 255, 255, 0.8), /* Putih lembut */
-        0 0 30px rgba(144, 238, 144, 0.8), /* Hijau pastel */
-        0 0 40px rgba(255, 215, 0, 0.8),  /* Emas lembut */
-        0 0 50px rgba(255, 140, 0, 0.8),  /* Oranye terang */
-        0 0 60px rgba(238, 130, 238, 0.8); /* Ungu pastel */
-    }
-
-    .nav-link:hover {
-      color: #000;
-      background: rgba(255, 255, 255, 0.1);
-      border: 2px solid #fffa65;
-      box-shadow: 0 0 15px rgba(255, 255, 255, 0.8),
-                  0 0 25px rgba(238, 130, 238, 0.7),
-                  0 0 35px rgba(255, 215, 0, 0.6),
-                  0 0 45px rgba(0, 255, 255, 0.7); /* Cyan neon */
-      transition: all 0.5s ease-in-out;
-    }
-
-    /* Efek border dengan warna shadow */
-    .nav-link.active {
-      border: 2px solid transparent;
-      box-shadow: 0 0 10px rgba(144, 238, 144, 0.8), /* Shadow Hijau Neon */
-                  0 0 20px rgba(255, 215, 0, 0.8),  /* Shadow Emas Neon */
-                  0 0 30px rgba(255, 140, 0, 0.8),  /* Shadow Oranye Neon */
-                  0 0 40px rgba(238, 130, 238, 0.8), /* Shadow Ungu Neon */
-                  0 0 50px rgba(0, 255, 255, 0.8); /* Shadow Cyan Neon */
-    }
           `}
         </style>
 
@@ -193,15 +151,21 @@ class TabsSection extends React.Component {
                     <NavItem>
                       <NavLink
                         aria-selected={this.state.plainTabs === 1}
-                        className={classnames("nav-link", {
+                        className={classnames("futuristik-nav-link", {
                           active: this.state.plainTabs === 1,
                         })}
                         onClick={(e) => this.toggleNavs(e, "plainTabs", 1)}
                         href="#pablo"
                         role="tab"
-                        style={tabStyle}
                       >
-                        <b>Profil</b>
+                        <b>PROFIL</b>
+                        {this.state.pieces[1]?.map((piece) => (
+                          <div
+                            key={`${piece.id}-${this.state.animationKeys[1]}`}
+                            className="fly-out"
+                            style={piece.style}
+                          ></div>
+                        ))}
                       </NavLink>
                     </NavItem>
                   </Col>
@@ -209,15 +173,21 @@ class TabsSection extends React.Component {
                     <NavItem>
                       <NavLink
                         aria-selected={this.state.plainTabs === 2}
-                        className={classnames("nav-link", {
+                        className={classnames("futuristik-nav-link", {
                           active: this.state.plainTabs === 2,
                         })}
                         onClick={(e) => this.toggleNavs(e, "plainTabs", 2)}
                         href="#pablo"
                         role="tab"
-                        style={tabStyle}
                       >
-                        <b>Layanan</b>
+                        <b>LAYANAN</b>
+                        {this.state.pieces[2]?.map((piece) => (
+                          <div
+                            key={`${piece.id}-${this.state.animationKeys[2]}`}
+                            className="fly-out"
+                            style={piece.style}
+                          ></div>
+                        ))}
                       </NavLink>
                     </NavItem>
                   </Col>
@@ -225,21 +195,28 @@ class TabsSection extends React.Component {
                     <NavItem>
                       <NavLink
                         aria-selected={this.state.plainTabs === 3}
-                        className={classnames("nav-link", {
+                        className={classnames("futuristik-nav-link", {
                           active: this.state.plainTabs === 3,
                         })}
                         onClick={(e) => this.toggleNavs(e, "plainTabs", 3)}
                         href="#pablo"
                         role="tab"
-                        style={tabStyle}
                       >
-                        <b>Transparansi</b>
+                        <b>TRANSPARANSI</b>
+                        {this.state.pieces[3]?.map((piece) => (
+                          <div
+                            key={`${piece.id}-${this.state.animationKeys[3]}`}
+                            className="fly-out"
+                            style={piece.style}
+                          ></div>
+                        ))}
                       </NavLink>
                     </NavItem>
                   </Col>
                 </Row>
               </Nav>
             </div>
+
             <TabContent activeTab={"plainTabs" + this.state.plainTabs}>
               <TabPane tabId="plainTabs1">
                 <Modals />
