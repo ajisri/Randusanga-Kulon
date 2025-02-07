2ce067d5ba1770ab13df30c93835b9c758f071ed
Ajis <ajisriyananta@gmail.com>
2025-02-07 15:43:59 +0700

    updatebackgroundhero

--- frontend/src/views/IndexSections/Hero.js
+++ frontend/src/views/IndexSections/Hero.js
@@ -283,7 +283,7 @@ const Hero = () => {
   left: 0;
   width: 100%;
   height: 100%;
-  background: linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.2));
   z-index: 2;
   pointer-events: none;
 }
