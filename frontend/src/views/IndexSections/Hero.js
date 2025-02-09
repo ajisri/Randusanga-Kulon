f9ebe99a0620cfba7f726ea9a45639d24ec2aca9
Ajis <ajisriyananta@gmail.com>
2025-02-09 18:24:25 +0700

    updatefliker

--- frontend/src/views/IndexSections/Hero.js
+++ frontend/src/views/IndexSections/Hero.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import ReactTypingEffect from "react-typing-effect";
 import Tabs from "./Tabs.js";
@@ -6,66 +12,58 @@ import Tabs from "./Tabs.js";
 const Hero = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [lasers, setLasers] = useState([]);
-  const [lasersLeft, setLasersLeft] = useState([]); // Laser dari kiri ke kanan
+  const [lasersLeft, setLasersLeft] = useState([]);
   const [isFast, setIsFast] = useState(false);
 
-  // Menambahkan laser baru secara acak tanpa pola
   const intervalRef = useRef(null);
 
-  // Efek Laser Random
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      setLasers((prevLasers) => {
-        const newLaser = {
-          id: Math.random().toString(36).substr(2, 9),
-          top: `${Math.random() * 100}%`,
-          left: `${Math.random() * 100}%`,
-        };
-        return [...prevLasers.slice(-49), newLaser];
-      });
-
-      setLasersLeft((prevLasers) => {
-        const newLaser = {
-          id: Math.random().toString(36).substr(2, 9),
-          top: `${Math.random() * 100}%`,
-          left: `${Math.random() * 10}%`,
-        };
-        return [...prevLasers.slice(-49), newLaser];
-      });
-    }, Math.random() * 500 + 300);
-
-    return () => clearInterval(intervalRef.current);
+  const generateLaser = useCallback(() => {
+    const newLaser = {
+      id: Math.random().toString(36).substr(2, 9),
+      top: `${Math.random() * 100}%`,
+      left: `${Math.random() * 100}%`,
+    };
+    return newLaser;
   }, []);
 
-  // Laser dari kiri ke kanan dengan kecepatan dinamis
+  const generateLaserLeft = useCallback(() => {
+    const newLaser = {
+      id: Math.random().toString(36).substr(2, 9),
+      top: `${Math.random() * 100}%`,
+      left: "0%",
+    };
+    return newLaser;
+  }, []);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setLasers((prevLasers) => [...prevLasers.slice(-49), generateLaser()]);
+      setLasersLeft((prevLasers) => [
+        ...prevLasers.slice(-49),
+        generateLaserLeft(),
+      ]);
+    }, 500); // Reduced interval time
+
+    return () => clearInterval(intervalRef.current);
+  }, [generateLaser, generateLaserLeft]);
+
   useEffect(() => {
     const interval = setInterval(
       () => {
         setLasersLeft((prevLasers) => [
-          ...prevLasers,
-          {
-            id: Math.random().toString(36).substr(2, 9), // ID unik
-            top: `${Math.random() * 100}%`, // Posisi vertikal acak
-            left: "0%", // Mulai dari sisi kiri
-          },
+          ...prevLasers.slice(-30),
+          generateLaserLeft(),
         ]);
-
-        if (lasersLeft.length > 30) {
-          setLasersLeft((prevLasers) => prevLasers.slice(-30));
-        }
       },
       isFast ? 300 : 1500
-    ); // Kecepatan berubah berdasarkan isFast
+    );
 
     return () => clearInterval(interval);
-  }, [lasersLeft, isFast]);
+  }, [isFast, generateLaserLeft]);
 
-  const [stars, setStars] = useState([]);
-
-  useEffect(() => {
-    // Membuat bintang hanya jika isFast true
+  const stars = useMemo(() => {
     if (isFast) {
-      const starElements = Array.from({ length: 50 }).map((_, i) => (
+      return Array.from({ length: 50 }).map((_, i) => (
         <div
           key={i}
           className="star"
@@ -77,10 +75,8 @@ const Hero = () => {
           }}
         />
       ));
-      setStars(starElements);
-    } else {
-      setStars([]);
     }
+    return [];
   }, [isFast]);
 
   const refaniFont = require("../../assets/font/Refani-Regular.otf");
@@ -89,89 +85,49 @@ const Hero = () => {
     <>
       <style>
         {`
-            @font-face {
-              font-family: 'Jaqueline';
-              src: url(${refaniFont}) format('truetype');
-            }
+          @font-face {
+            font-family: 'Jaqueline';
+            src: url(${refaniFont}) format('truetype');
+          }
 
           @keyframes shakeText {
-            0% {
-              transform: translateX(0);
-            }
-            20% {
-              transform: translateX(-1px); /* Pergeseran sedikit ke kiri */
-            }
-            40% {
-              transform: translateX(1px); /* Pergeseran sedikit ke kanan */
-            }
-            60% {
-              transform: translateX(-1px); /* Pergeseran sedikit ke kiri */
-            }
-            80% {
-              transform: translateX(1px); /* Pergeseran sedikit ke kanan */
-            }
-            100% {
-              transform: translateX(0);
-            }
+            0% { transform: translateX(0); }
+            20% { transform: translateX(-1px); }
+            40% { transform: translateX(1px); }
+            60% { transform: translateX(-1px); }
+            80% { transform: translateX(1px); }
+            100% { transform: translateX(0); }
           }
 
           @keyframes gradientMove {
-            0% {
-              background-position: 0% 50%;
-            }
-            50% {
-              background-position: 100% 50%;
-            }
-            100% {
-              background-position: 0% 50%;
-            }
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
           }
 
           @keyframes laserBeamInside {
-            0% {
-              left: 0; /* Mulai di kiri tombol */
-              opacity: 1;
-            }
-            100% {
-              left: 100%; /* Bergerak hingga ujung kanan tombol */
-              opacity: 0;
-            }
+            0% { left: 0; opacity: 1; }
+            100% { left: 100%; opacity: 0; }
           }
 
           @keyframes spaceStars {
-            0% {
-              background-position: 0 0;
-            }
-            50% {
-              background-position: 50% 50%;
-            }
-            100% {
-              background-position: 0 0;
-            }
+            0% { background-position: 0 0; }
+            50% { background-position: 50% 50%; }
+            100% { background-position: 0 0; }
           }
 
           @keyframes randomStars {
-            0% {
-              background-position: 0 0;
-            }
-            25% {
-              background-position: 100px 50px;
-            }
-            50% {
-              background-position: -100px -50px;
-            }
-            75% {
-              background-position: 200px -100px;
-            }
-            100% {
-              background-position: 0 0;
-            }
+            0% { background-position: 0 0; }
+            25% { background-position: 100px 50px; }
+            50% { background-position: -100px -50px; }
+            75% { background-position: 200px -100px; }
+            100% { background-position: 0 0; }
           }
-            
+
           .laser-left {
             position: absolute;
-            width: 20px; /* Lebar memanjang */
-            height: 5px; /* Tinggi lebih kecil */
+            width: 20px;
+            height: 5px;
             background: #ffffff;
             border-radius: 5px;
             animation: laserBeamInside 2s linear forwards;
@@ -179,45 +135,44 @@ const Hero = () => {
           }
 
           .section-custom::after {
-              content: '';
-              position: absolute;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 50%;
-              background: 
-                radial-gradient(circle, rgba(255, 255, 255, 0.5) 0%, transparent 1%) 10% 20%, 
-                radial-gradient(circle, rgba(255, 255, 255, 0.3) 0%, transparent 1%) 50% 50%, 
-                radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 80% 30%, 
-                radial-gradient(circle, rgba(255, 255, 255, 0.6) 0%, transparent 1%) 15% 80%, 
-                radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 40% 60%;
-              background-size: 150px 150px; /* Ukuran acak */
-              pointer-events: none;
-              z-index: 1; /* Agar berada di belakang konten lainnya */
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 50%;
+            background: 
+              radial-gradient(circle, rgba(255, 255, 255, 0.5) 0%, transparent 1%) 10% 20%, 
+              radial-gradient(circle, rgba(255, 255, 255, 0.3) 0%, transparent 1%) 50% 50%, 
+              radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 80% 30%, 
+              radial-gradient(circle, rgba(255, 255, 255, 0.6) 0%, transparent 1%) 15% 80%, 
+              radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 40% 60%;
+            background-size: 150px 150px;
+            pointer-events: none;
+            z-index: 1;
           }
 
-.overlay-gradient-top {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 50%;
-  background: linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
-  z-index: 2;
-  pointer-events: none;
-}
+          .overlay-gradient-top {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 50%;
+            background: linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
+            z-index: 2;
+            pointer-events: none;
+          }
 
-/* Overlay bagian bawah (fade in ke gelap) */
-.overlay-gradient-bottom {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 50%;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
-  z-index: 2;
-  pointer-events: none;
-}
+          .overlay-gradient-bottom {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 50%;
+            background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
+            z-index: 2;
+            pointer-events: none;
+          }
 
           .section-hero {
             position: relative;
@@ -229,10 +184,10 @@ const Hero = () => {
 
           .hero-title {
             font-family: 'Jaqueline', sans-serif;
-            font-size: 2.8rem; /* Font lebih besar dan dominan */
-            font-weight: 800; /* Font tebal */
-            color: #ffffff !important
-            text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);;
+            font-size: 2.8rem;
+            font-weight: 800;
+            color: #ffffff !important;
+            text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
             text-align: center;
             text-transform: uppercase;
             line-height: 1.4;
@@ -243,9 +198,9 @@ const Hero = () => {
           .btn-custom {
             overflow: hidden;
             font-family: 'Jaqueline', sans-serif;
-            font-size: 1.2rem; /* Font lebih besar */
+            font-size: 1.2rem;
             padding: 20px 40px;
-            background: linear-gradient(90deg, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1)); /* Gradien transparan dari hitam */
+            background: linear-gradient(90deg, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1));
             border: none;
             color: white;
             text-transform: uppercase;
@@ -258,8 +213,8 @@ const Hero = () => {
           }
 
           .btn-custom:hover {
-            background: rgba(0, 0, 0, 0); /* Background sepenuhnya transparan saat hover */
-            color: white; /* Warna teks tetap putih */
+            background: rgba(0, 0, 0, 0);
+            color: white;
           }
 
           .btn-custom::after {
@@ -275,25 +230,24 @@ const Hero = () => {
               radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 80% 30%, 
               radial-gradient(circle, rgba(255, 255, 255, 0.6) 0%, transparent 1%) 15% 80%, 
               radial-gradient(circle, rgba(255, 255, 255, 0.4) 0%, transparent 1%) 40% 60%;
-            background-size: 150px 150px; /* Ukuran acak */
+            background-size: 150px 150px;
             pointer-events: none;
           }
 
           .btn-custom.active {
-            background: #000000 !important; /* Latar belakang hitam */
-            color: #ffffff; /* Warna teks tetap putih */
+            background: #000000 !important;
+            color: #ffffff;
             font-size: 1rem;
-            text-shadow: 0 0 3px #ffffff, 0 0 5px #ffffff, 0 0 10px rgba(255, 255, 255, 0.6); /* Mengurangi efek cahaya bintang */
-            border: 2px solid rgba(255, 255, 255, 0.6); /* Mengurangi transparansi border */
-            box-shadow: 0 0 10px 2px rgba(255, 255, 255, 0.5), 0 0 15px 5px rgba(144, 238, 144, 0.5), 0 0 20px 5px rgba(255, 215, 0, 0.4); /* Mengurangi efek cahaya */
-            /* animation: movingNeonBorder 6s linear infinite, spaceStars 10s infinite; */ /* Memperlambat animasi */
-            animation: spaceStars 10s infinite; /* Memperlambat animasi */
+            text-shadow: 0 0 3px #ffffff, 0 0 5px #ffffff, 0 0 10px rgba(255, 255, 255, 0.6);
+            border: 2px solid rgba(255, 255, 255, 0.6);
+            box-shadow: 0 0 10px 2px rgba(255, 255, 255, 0.5), 0 0 15px 5px rgba(144, 238, 144, 0.5), 0 0 20px 5px rgba(255, 215, 0, 0.4);
+            animation: spaceStars 10s infinite;
             transform: scale(0.95);
           }
 
           .btn-custom.active span {
-            display: inline-block; /* Teks bertindak sebagai elemen blok */
-            animation: shakeText 0.5s ease-in-out infinite; /* Animasi teks */
+            display: inline-block;
+            animation: shakeText 0.5s ease-in-out infinite;
           }
 
           .subtitle {
@@ -312,7 +266,7 @@ const Hero = () => {
             width: 100%;
             height: 50%;
             z-index: 2;
-            perspective: 1000px; /* Menambahkan perspektif 3D */
+            perspective: 1000px;
             overflow: hidden;
           }
 
@@ -338,6 +292,7 @@ const Hero = () => {
               transform: translateZ(-1000px) translateY(calc(100vh * (random() - 0.5))) translateX(calc(100vw * (random() - 0.5)));
               opacity: 0;
             }
+          }
         `}
       </style>
 
@@ -356,7 +311,7 @@ const Hero = () => {
               height: "100%",
               objectFit: "cover",
               zIndex: 1,
-              filter: isFast ? "brightness(10%)" : "brightness(100%)", // Ubah kecerahan saat tombol aktif
+              filter: isFast ? "brightness(10%)" : "brightness(100%)",
             }}
             autoPlay
             loop
@@ -414,22 +369,6 @@ const Hero = () => {
                     position: "relative",
                   }}
                 >
-                  {/* Laser Beams */}
-                  {lasers.map((laser) => (
-                    <span
-                      key={laser.id}
-                      className="laser"
-                      style={{
-                        position: "absolute",
-                        width: "2px",
-                        height: "2x",
-                        top: laser.top,
-                        left: laser.left,
-                      }}
-                    ></span>
-                  ))}
-
-                  {/* Laser Beams from Left */}
                   {lasers.map((laser) => (
                     <span
                       key={laser.id}
@@ -443,10 +382,24 @@ const Hero = () => {
                       }}
                     ></span>
                   ))}
+
+                  {lasersLeft.map((laser) => (
+                    <span
+                      key={laser.id}
+                      className="laser-left"
+                      style={{
+                        position: "absolute",
+                        width: "2px",
+                        height: "2px",
+                        top: laser.top,
+                        left: laser.left,
+                      }}
+                    ></span>
+                  ))}
                   <Button
                     onClick={() => {
                       setIsVisible(!isVisible);
-                      setIsFast(!isFast); // Toggle the laser speed
+                      setIsFast(!isFast);
                     }}
                     className={`btn-custom ${isFast ? "active" : ""}`}
                     block
