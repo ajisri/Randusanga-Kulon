c99f3eca4aa9b78c7607ef0c738a349e463a8711
Ajis <ajisriyananta@gmail.com>
2025-02-22 12:36:38 +0700

    updateasuwww

--- frontend/src/views/IndexSections/Modals.js
+++ frontend/src/views/IndexSections/Modals.js
@@ -719,7 +719,7 @@ const Modals = () => {
                 alignItems: "center",
                 justifyContent: "center",
                 flex: 1,
-                width: "50px",
+                width: "55px",
               }}
             >
               <img
