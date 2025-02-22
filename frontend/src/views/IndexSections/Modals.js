ca08aad9f120ffaf4e0914ab0c78c7d2585116c8
Ajis <ajisriyananta@gmail.com>
2025-02-23 05:35:20 +0700

    ndangsuv

--- frontend/src/views/IndexSections/Modals.js
+++ frontend/src/views/IndexSections/Modals.js
@@ -735,7 +735,7 @@ const Modals = () => {
             }
 
             .button-icon {
-                width: 50%;
+                width: 33.33vh !important;
                 height: 80%;
                 min-height: 60px; /* Lebih kecil untuk layar sempit */
             }
