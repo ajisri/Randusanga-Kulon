d0f380ece62e29d5bbe2d70be37fdee2ed4c5ffe
ajisri <ajisriyananta@gmail.com>
2025-02-27 11:46:00 +0700

    updateasu2

--- frontend/src/views/IndexSections/Hero.js
+++ frontend/src/views/IndexSections/Hero.js
@@ -245,7 +245,7 @@ const Hero = () => {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    height: "100vh",
+                    minHeight: "200px",
                     backgroundColor: "transparent",
                   }}
                 >
