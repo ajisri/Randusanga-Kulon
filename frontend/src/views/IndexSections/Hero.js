61dae4342c945ce8079b41598bc272ae3815ba59
Ajis <ajisriyananta@gmail.com>
2025-03-02 11:13:42 +0700

    hero48

--- frontend/src/views/IndexSections/Hero.js
+++ frontend/src/views/IndexSections/Hero.js
@@ -236,7 +236,7 @@ const Hero = () => {
               <Col
                 md={4}
                 style={{
-                  padding: "20px 2px 80px 2px", // Padding hanya di atas dan bawah
+                  padding: "20px 0 80px 0", // Padding hanya di atas dan bawah
                   display: "flex",
                   flexDirection: "column",
                   justifyContent: "flex-start",
@@ -270,7 +270,7 @@ const Hero = () => {
                   style={{
                     width: "100%",
                     height: "auto", // Tinggi menyesuaikan konten
-                    margin: "0", // Margin dihapus
+                    margin: "0 5px", // Margin dihapus
                     overflow: "visible", // Nonaktifkan scroll
                   }}
                 >
